bdfe61e8db18ca5927c20f2c22556b617faebe9d
lilifernandez91 <lilibetfdez@gmail.com>
2023-07-10 20:08:31 +0200

    added new features

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,38 @@
-// variable para almacenar puntuacion
+import * as confetti from 'canvas-confetti';
+
+const btnDameCarta = document.getElementById('dame')
+const elementoPuntuacion = document.getElementById('puntuacion')
+const elementoCarta = document.getElementById('carta')
+const mensajes = document.getElementById('mensajes')
+const mePlanto = document.getElementById('planto')
+const comenzar = document.getElementById('comenzar')
+
+
 let puntuacion: number = 0
 
-// variable para guardar un array de numeros
 const cartas: Array<number> = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12]
 
-// enlaces a las imágenes
 const imageSrc = '/images/{carta}-copas.jpg'
 const backImageSrc = '/images/back.jpg'
 
-// funcion para mostrar la puntuacion
-const muestraPuntuacion = () => {
-    const elementoPuntuacion = document.getElementById('puntuacion')
-    if (elementoPuntuacion) {
-        elementoPuntuacion.innerHTML = puntuacion.toString()
-    }
-}
-muestraPuntuacion()
-
-document.addEventListener('DOMContentLoaded', () => {
-    muestraPuntuacion()
-})
-
-// funcion para generar una carta aleatoria
-const generarCartaAleatoria = () => {
-    const indiceAleatorio = Math.floor(Math.random() * cartas.length)
-    return cartas[indiceAleatorio]
-}
-
-// funcion que nos devuelve una carta aleatoria
 const dameCarta = () => {
     const cartaAleatoria = generarCartaAleatoria()
     calcularPuntuacion(cartaAleatoria)
     muestraPuntuacion()
     muestraCarta(cartaAleatoria)
-}
-
-const calcularPuntuacion = (cartaAleatoria: number) => {
-    switch (cartaAleatoria) {
-        case 10:
-        case 11:
-        case 12:
-            puntuacion += 0.5
-            break
-        default:
-            puntuacion += cartaAleatoria
-            break
+    gameOver()
+    if (mensajes && btnDameCarta && btnDameCarta instanceof HTMLButtonElement && puntuacion === 7.5) {
+        mensajes.innerHTML = '¡Lo has clavado! ¡Enhorabuena!'
+        confetti.default()
+        btnDameCarta.disabled = true
     }
 }
 
-const btnDameCarta = document.getElementById('dame')
-if (btnDameCarta) {
-    btnDameCarta.addEventListener('click', dameCarta)
+const generarCartaAleatoria = (): number => {
+    const indiceAleatorio = Math.floor(Math.random() * cartas.length)
+    return cartas[indiceAleatorio]
 }
 
-// funcion para mostrar la carta
 const muestraCarta = (carta: number): void => {
     let imagen: string
 
@@ -74,11 +53,73 @@ const muestraCarta = (carta: number): void => {
             imagen = backImageSrc
             break
     }
-    const elementoCarta = document.getElementById('carta')
     if (elementoCarta) {
         elementoCarta.setAttribute('src', imagen)
     }
 }
+
+const muestraPuntuacion = (): void => {
+    if (elementoPuntuacion) {
+        elementoPuntuacion.innerHTML = puntuacion.toString()
+    }
+}
+
+const calcularPuntuacion = (cartaAleatoria: number): void => {
+    switch (cartaAleatoria) {
+        case 10:
+        case 11:
+        case 12:
+            puntuacion += 0.5
+            break
+        default:
+            puntuacion += cartaAleatoria
+            break
+    }
+}
+
+const gameOver = (): void => {
+    if (puntuacion > 7.5 && mensajes && btnDameCarta && btnDameCarta instanceof HTMLButtonElement) {
+        mensajes.innerHTML = 'GAME OVER: LO SENTIMOS, LA PUNTUACION DEBE SER IGUAL O MENOR QUE 7.5'
+        btnDameCarta.disabled = true
+    }
+}
+
+const handleMePlanto = (): void => {
+    if (btnDameCarta && btnDameCarta instanceof HTMLButtonElement && mensajes) {
+        btnDameCarta.disabled = true
+        if (puntuacion === 0) {
+            mensajes.innerHTML = 'Por favor, pide una carta'
+            btnDameCarta.disabled = false
+        }
+    } if (mensajes) {
+        if (puntuacion >= 0.5 && puntuacion < 4) {
+            mensajes.innerHTML = 'Has sido muy conservador'
+        } if (puntuacion >= 4 && puntuacion < 6) {
+            mensajes.innerHTML = 'Te ha entrado el canguelo eh?'
+        } if (puntuacion >= 6 && puntuacion < 7.5) {
+            mensajes.innerHTML = 'Casi casi....'
+        }
+    }
+}
+
+const handleComenzar = (): void => {
+    puntuacion = 0
+
+    if (elementoPuntuacion && elementoCarta && btnDameCarta && btnDameCarta instanceof HTMLButtonElement && mensajes) {
+        elementoPuntuacion.innerHTML = puntuacion.toString()
+        elementoCarta.setAttribute('src', backImageSrc)
+        btnDameCarta.disabled = false
+        mensajes.innerHTML = ''
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (mePlanto && btnDameCarta && comenzar) {
+        mePlanto.addEventListener('click', handleMePlanto)
+        btnDameCarta.addEventListener('click', dameCarta)
+        comenzar.addEventListener('click', handleComenzar)
+    }
+})
 
 
 
